f2c49355e1533d392fabe0d97be3ad416f15e45a
Ayussh Verma <ayussh.verma@maersk.com>
2024-08-27 23:57:11 +0530

    User: Add a DELETE method which returns a count

--- src/main/java/com/ayusshverma/discord_message_store/repos/UserRepo.java
+++ src/main/java/com/ayusshverma/discord_message_store/repos/UserRepo.java
@@ -1,9 +1,18 @@
 package com.ayusshverma.discord_message_store.repos;
 
 import com.ayusshverma.discord_message_store.models.User;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, String> {
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM User u WHERE u.id = ?1")
+    int deleteByIdReturningCount(String id);
 }
