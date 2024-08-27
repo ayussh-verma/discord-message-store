773e17c204240279401f880afcfcf6506c3235de
Ayussh Verma <ayussh.verma@maersk.com>
2024-08-27 14:08:26 +0530

    Initial commit

---
+++ src/main/java/com/ayusshverma/discord_message_store/DiscordMessageStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ayusshverma.discord_message_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordMessageStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordMessageStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayusshverma/discord_message_store/DiscordMessageStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayusshverma.discord_message_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordMessageStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
