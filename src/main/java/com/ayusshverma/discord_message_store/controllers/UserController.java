bd04008b922275b1d9d4c68d58677076c4270c6d
Ayussh Verma <ayussh.verma@maersk.com>
2024-08-28 09:48:08 +0530

    Add API documentation for GET /users/{id}

--- src/main/java/com/ayusshverma/discord_message_store/controllers/UserController.java
+++ src/main/java/com/ayusshverma/discord_message_store/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.ayusshverma.discord_message_store.controllers;
 import com.ayusshverma.discord_message_store.models.User;
 import com.ayusshverma.discord_message_store.repos.UserRepo;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +26,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Tag(name = "User", description = "The User API")
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -33,6 +42,13 @@ public class UserController {
         return ResponseEntity.ok(userRepo.findAll());
     }
 
+    @Operation(summary = "Get a specific user", description = "Returns a single user", tags = { "User" })
+    @ApiResponses(
+        value = {
+            @ApiResponse(responseCode = "200", description = "User found", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = User.class)) }),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content)
+        }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<User> getASpecificUser(@PathVariable String id) {
         Optional<User> user = userRepo.findById(id);
@@ -58,6 +74,11 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user, UriComponentsBuilder uriComponentsBuilder) {
+        Optional<User> currentUser = userRepo.findById(user.getId());
+        if (!currentUser.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
         User createdUser = userRepo.save(user);
         URI location = uriComponentsBuilder.path("/users/{id}").buildAndExpand(createdUser.getId()).toUri();
         return ResponseEntity.created(location).body(createdUser);
